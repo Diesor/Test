462e24781dc946927f9ece8bb65573b1d30bbf95
Diesor <dsoria03@yahoo.com>
2024-11-28 18:59:10 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+# Author: Diego Soria <dsoria03@gmail.com>
+
+sudo apt update
+sudo apt install snapd
+
+# Visual Studio Code
+sudo snap install code --classic
+
+#Netbeans
+sudo snap install netbeans --classic
+
+#IntelliJ
+sudo snap install intellij-idea-community --classic
+
+
+
+
